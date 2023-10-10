51a83fa6eb07dd14d77b312bdbb08e18f5c7a70f
xarshm <66797826+xarshm@users.noreply.github.com>
2023-10-10 11:20:55 +0530

    Update cart.js

--- pages/cart.js
+++ pages/cart.js
@@ -68,7 +68,7 @@ const Cart = () => {
                                     className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center"
                                 >
                                     Checkout
-                                    {loading && <img src="/spinner.svg" />}
+                                    // {loading && <img src="/spinner.svg" />}
                                 </button>
                                 {/* BUTTON END */}
                             </div>
